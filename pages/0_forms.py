afe90a9c0d98337b46a516132171d3a6860f2cb4
Mr-bob-kou <bob323565@gmail.com>
2024-11-29 15:46:07 +0800

    Update 0_forms.py

--- pages/0_forms.py
+++ pages/0_forms.py
@@ -16,4 +16,4 @@ st.sidebar.image(logo)
 data1="https://github.com/Mr-bob-kou/My_Respository/raw/main/World%20Heritage%20Counts.geojson"
 st.title("Marker Cluster")
 gdf = gpd.read_file(data1)
-st.dataframe(data=data1)
+st.dataframe(data=gdf)
