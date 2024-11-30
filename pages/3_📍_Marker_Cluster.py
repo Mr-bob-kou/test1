fd6debd388c2606aab1b4cb3d9650352122fe70e
Mr-bob-kou <bob323565@gmail.com>
2024-11-30 14:17:21 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -15,7 +15,7 @@ logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
 st.title("Marker Cluster")
-data="https://raw.githubusercontent.com/Mr-bob-kou/My_Respository/refs/heads/main/worldheri/worldheritagesites.shp"
+data="https://raw.githubusercontent.com/Mr-bob-kou/My_Respository/main/worldheri/worldheritagesites.shp"
 heritage=gpd.read_file(data)
 with st.expander("See source code"):
     with st.echo():
