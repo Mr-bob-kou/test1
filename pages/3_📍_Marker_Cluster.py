a3b2f9942063c9c600ad8bbba1a5c953729924af
Mr-bob-kou <bob323565@gmail.com>
2024-11-30 14:40:25 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -18,7 +18,7 @@ st.title("Marker Cluster")
 data="https://raw.githubusercontent.com/Mr-bob-kou/My_Respository/main/point.geojson"
 heritage=gpd.read_file(data)
 with st.expander("See source code"):
-    heritage.dataframe(data=gdf)
+    st.dataframe(data=heritage)
 
 m = leafmap.Map(center=[40, -100], zoom=4)
 regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
