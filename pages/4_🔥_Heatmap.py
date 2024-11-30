a3675186f7f85ba93790749b53c924e50882f3b2
Mr-bob-kou <bob323565@gmail.com>
2024-11-30 15:43:40 +0800

    Update 4_🔥_Heatmap.py

--- pages/4_🔥_Heatmap.py
+++ pages/4_🔥_Heatmap.py
@@ -15,17 +15,17 @@ logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
 st.title("Heatmap")
-data="https://raw.githubusercontent.com/Mr-bob-kou/My_Respository/main/point.geojson"
-heritage=gpd.read_file(data)
+
 with st.expander("See source code"):
     with st.echo():
+        data="https://raw.githubusercontent.com/Mr-bob-kou/My_Respository/refs/heads/main/point2.geojson"
+        heritage=gpd.read_file(data)
         m = leafmap.Map(center=[40, -100], zoom=4)
         m.add_heatmap(
-            heritage,
-            latitude="LATITUDE",
-            longitude="LONGITUDE",
-            value=1,
-            name="Heat map",
-            radius=20,
-        )
+        heritage,
+        latitude="LATITUDE",
+        longitude="LONGITUDE",
+        value="AREAHA",
+        name="Heat map",
+        radius=20)
 m.to_streamlit(height=700)
