2dbe5a8ae65bbff9d53c271bdb3b3b188c9d4a66
Mr-bob-kou <bob323565@gmail.com>
2024-12-08 16:47:23 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -4,26 +4,18 @@ import geopandas as gpd
 
 st.set_page_config(layout="wide")
 
-markdown = """
-A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
-"""
 
-st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
-st.sidebar.image(logo)
-
-st.title("Marker Cluster")
+col1, col2 = st.columns([4, 1])
+st.title("General")
 data="https://raw.githubusercontent.com/Mr-bob-kou/My_Respository/main/point.geojson"
+regions = "https://raw.githubusercontent.com/Mr-bob-kou/My_Respository/main/world-administrative-boundaries.geojson"
 heritage=gpd.read_file(data)
 with st.expander("See All Heritage Data"):
     st.dataframe(data=heritage)
-
-m = leafmap.Map(center=[40, -100], zoom=4)
-regions = "https://raw.githubusercontent.com/Mr-bob-kou/My_Respository/main/world-administrative-boundaries.geojson"
-
-m.add_geojson(regions, layer_name="Countries")
-m.add_points_from_xy(heritage,x="LONGITUDE",y="LATITUDE", popup=["NAME","DATEINSCRI","COUNTRY","DESCRIPTIO","AREAHA","DANGER","LONGITUDE","LATITUDE"])
-
-m.to_streamlit(height=700)
+with col2:
+    st.write("Blank Space")
+with col1:
+    m = leafmap.Map(center=[40, -100], zoom=4)
+    m.add_geojson(regions, layer_name="Countries")
+    m.add_points_from_xy(heritage,x="LONGITUDE",y="LATITUDE", popup=["NAME","DATEINSCRI","COUNTRY","DESCRIPTIO","AREAHA","DANGER","LONGITUDE","LATITUDE"])
+    m.to_streamlit(height=700)
