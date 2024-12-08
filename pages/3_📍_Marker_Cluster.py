d581f5dbe9301bb8d29f261c519dee1331617c8e
Mr-bob-kou <bob323565@gmail.com>
2024-12-08 17:37:39 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -8,12 +8,28 @@ st.set_page_config(layout="wide")
 st.title("General")
 data="https://raw.githubusercontent.com/Mr-bob-kou/My_Respository/main/point.geojson"
 regions = "https://raw.githubusercontent.com/Mr-bob-kou/My_Respository/main/world-administrative-boundaries.geojson"
+data2="https://github.com/Mr-bob-kou/My_Respository/raw/main/World%20Heritage%20Counts.geojson"
 
 options = list(leafmap.basemaps.keys())
 index = options.index("OpenTopoMap")
 modes=["預設","熱力圖","總數統計圖"]
 modes1="預設"
-
+def style_function(feature):
+    population = data2['properties']['count']
+    
+    if count > 20:
+        color = 'red'
+    elif count > 10:
+        color = 'orange'
+    else:
+        color = 'green'
+    
+    return {
+        'fillColor': color,
+        'color': color,
+        'weight': 2,
+        'fillOpacity': 0.5
+    }
 with st.expander("See All Heritage Data"):
     heritage=gpd.read_file(data)
     st.dataframe(data=heritage)
@@ -26,12 +42,11 @@ with col2:
 with col1:
     m = leafmap.Map(center=[40, -100], zoom=4)
     if mode=='總數統計圖':
-        data="https://github.com/Mr-bob-kou/My_Respository/raw/main/World%20Heritage%20Counts.geojson"
         m = leafmap.Map(
             locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
         )
         m.add_basemap(basemap)
-        m.add_geojson(data)
+        m.add_geojson(data2,style_callback=style_function)
         m.to_streamlit(height=700)
     else:
         m.add_geojson(regions, layer_name="Countries")
