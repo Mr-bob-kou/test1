941d42ede2543bde81434edc8c233f5430e1b22a
Mr-bob-kou <bob323565@gmail.com>
2024-12-08 16:54:12 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -9,17 +9,20 @@ st.title("General")
 data="https://raw.githubusercontent.com/Mr-bob-kou/My_Respository/main/point.geojson"
 regions = "https://raw.githubusercontent.com/Mr-bob-kou/My_Respository/main/world-administrative-boundaries.geojson"
 
+options = list(leafmap.basemaps.keys())
+index = options.index("OpenTopoMap")
+
 with st.expander("See All Heritage Data"):
     heritage=gpd.read_file(data)
     st.dataframe(data=heritage)
 col1, col2 = st.columns([4, 1])
 
-options = list(leafmap.basemaps.keys())
-index = options.index("OpenTopoMap")
+
 with col2:
     basemap = st.selectbox("Select a basemap:", options, index)
 with col1:
     m = leafmap.Map(center=[40, -100], zoom=4)
     m.add_geojson(regions, layer_name="Countries")
     m.add_points_from_xy(heritage,x="LONGITUDE",y="LATITUDE", popup=["NAME","DATEINSCRI","COUNTRY","DESCRIPTIO","AREAHA","DANGER","LONGITUDE","LATITUDE"])
+    m.add_basemap(basemap)
     m.to_streamlit(height=700)
