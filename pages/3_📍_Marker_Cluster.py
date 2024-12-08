8430760e0dbc63d846779e40d475df54bd2bb885
Mr-bob-kou <bob323565@gmail.com>
2024-12-08 16:50:23 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -8,12 +8,17 @@ st.set_page_config(layout="wide")
 st.title("General")
 data="https://raw.githubusercontent.com/Mr-bob-kou/My_Respository/main/point.geojson"
 regions = "https://raw.githubusercontent.com/Mr-bob-kou/My_Respository/main/world-administrative-boundaries.geojson"
-heritage=gpd.read_file(data)
+
+options = list(leafmap.basemaps.keys())
+index = options.index("OpenTopoMap")
+
 with st.expander("See All Heritage Data"):
+    heritage=gpd.read_file(data)
     st.dataframe(data=heritage)
 col1, col2 = st.columns([4, 1])
+
 with col2:
-    st.write("Blank Space")
+    basemap = st.selectbox("Select a basemap:", options, index)
 with col1:
     m = leafmap.Map(center=[40, -100], zoom=4)
     m.add_geojson(regions, layer_name="Countries")
