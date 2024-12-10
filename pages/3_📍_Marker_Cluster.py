6aac74eab5715f37839926e5f71bb6af6d055452
Mr-bob-kou <bob323565@gmail.com>
2024-12-10 16:30:55 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -105,7 +105,7 @@ with col1:
         pop=["NAME","DATEINSCRI","COUNTRY","DESCRIPTIO","AREAHA","DANGER","LONGITUDE","LATITUDE"]
         Default(heritage,m1, "LONGITUDE","LATITUDE",pop)
     elif mode=="Inscribed Date":
-        m=leafmap.Map(center=[40, -100], zoom=4))
+        m=leafmap.Map(center=[40, -100], zoom=4)
         Insc=heritage['DATEINSCRI']=Inscdate
         m.add_geojson(regions, layer_name="Countries")
         m.add_points_from_xy(Insc,x="LONGITUDE",y="LATITUDE", popup=["NAME","DATEINSCRI","COUNTRY","DESCRIPTIO","AREAHA","DANGER","LONGITUDE","LATITUDE"])
