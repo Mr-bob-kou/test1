349ee551693dd1f0ad2f5b56412d2b8ba127ee69
Mr-bob-kou <bob323565@gmail.com>
2024-12-10 18:14:20 +0800

    Create Adding_Some_Heritages.py

---
+++ pages/Adding_Some_Heritages.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+import geopandas as gpd
+import altair as alt
+
+st.set_page_config(layout="wide")
+
+
+st.title("Adding!!")
+st.write("Coming Soon......")
