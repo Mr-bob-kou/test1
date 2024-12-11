009b6c8d1bf2843eaadf707390c6d7cae75d8c61
Mr-bob-kou <bob323565@gmail.com>
2024-12-11 14:55:08 +0800

    Update Heritages_Near_Me.py

--- pages/Heritages_Near_Me.py
+++ pages/Heritages_Near_Me.py
@@ -9,7 +9,7 @@ st.set_page_config(layout="wide")
 st.title("Heritage Near Me")
 data="https://raw.githubusercontent.com/Mr-bob-kou/My_Respository/main/point.geojson"
 heritage=gpd.read_file(data)
-m1 = leafmap.Map(center=[40, -100], zoom=4,locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
+mp = leafmap.Map(center=[40, -100], zoom=4,locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
 pop=["NAME","DATEINSCRI","COUNTRY","DESCRIPTIO","AREAHA","DANGER","LONGITUDE","LATITUDE"]
 mp.add_geojson(regions, layer_name="Countries")
 mp.add_points_from_xy(datum,x=lon,y=lat, popup=pop)
