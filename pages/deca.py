2f31cd34d739753c25ba862bf576e07761648d0b
Mr-bob-kou <bob323565@gmail.com>
2024-12-11 14:01:59 +0800

    Create deca.py

---
+++ pages/deca.py
@@ -0,0 +1,19 @@
+import altair as alt
+import pandas as pd
+
+# 創建範例數據
+data = pd.DataFrame({
+    'category': ['A', 'B', 'C', 'D', 'E'],
+    'value': [10, 30, 50, 20, 60]
+})
+
+# 使用條件語句來設置顏色
+chart = alt.Chart(data).mark_bar().encode(
+    x='category',
+    y='value',
+    color=alt.condition(
+        alt.datum.value > 40,  # 如果 value 大於 40，則應用紅色
+        alt.value('red'),      # 這是條件為真時的顏色
+        alt.value('steelblue') # 這是條件為假時的顏色
+    )
+)
