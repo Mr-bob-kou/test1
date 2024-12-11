d0a34a491ffe4480b83ce6b2eaa3f57ba9b7bf00
Mr-bob-kou <bob323565@gmail.com>
2024-12-11 14:57:10 +0800

    Update Heritages_Near_Me.py

--- pages/Heritages_Near_Me.py
+++ pages/Heritages_Near_Me.py
@@ -13,6 +13,6 @@ regions = "https://raw.githubusercontent.com/Mr-bob-kou/My_Respository/main/worl
 mp = leafmap.Map(center=[40, -100], zoom=4,locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
 pop=["NAME","DATEINSCRI","COUNTRY","DESCRIPTIO","AREAHA","DANGER","LONGITUDE","LATITUDE"]
 mp.add_geojson(regions, layer_name="Countries")
-mp.add_points_from_xy(heritage,x=lon,y=lat, popup=pop)
+mp.add_points_from_xy(heritage,x="LONGITUDE",y="LATITUDE", popup=pop)
 mp.add_basemap(basemap)
 mp.to_streamlit(height=700)
