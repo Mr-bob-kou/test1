3b61c79e62bf217eaa60097d146613a14b662c29
Mr-bob-kou <bob323565@gmail.com>
2024-12-11 12:51:12 +0800

    Update 3_📍_Main_Platform.py

--- pages/3_📍_Main_Platform.py
+++ pages/3_📍_Main_Platform.py
@@ -119,7 +119,7 @@ with col1:
         pop=["NAME","DATEINSCRI","COUNTRY","DESCRIPTIO","AREAHA","DANGER","LONGITUDE","LATITUDE"]
         Default(heritage,m1, "LONGITUDE","LATITUDE",pop)
         st.write("test")
-        st.write("last_object_clicked_tooltip")
+        st.write(m1["last_object_clicked_tooltip"])
     elif mode=="Inscribed Date":
         m=leafmap.Map(center=[40, -100], zoom=4)
         Insc=heritage[heritage['DATEINSCRI']==Inscdate]
