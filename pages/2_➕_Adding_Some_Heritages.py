a520b4b633c0f4319cdb2d65c271f06dafc99a4f
Mr-bob-kou <bob323565@gmail.com>
2024-12-14 23:24:28 +0800

    Update 2_➕_Adding_Some_Heritages.py

--- pages/2_➕_Adding_Some_Heritages.py
+++ pages/2_➕_Adding_Some_Heritages.py
@@ -8,5 +8,11 @@ st.set_page_config(layout="wide")
 
 st.title("Adding!!")
 st.write("Coming Soon......")
-txt = st.text_area("Text to analyze",)
-st.write(txt)
+with st.form("my_form"):
+    st.write("Inside the form")
+    name = st.text_input("Name")
+    Country= st.text_input("Country")
+
+    submitted = st.form_submit_button("Submit")
+if submitted:
+    st.write("Name", name, "Country", country)
