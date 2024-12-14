afaa2dbcab4494eda91416fae59fe50107f376d5
Mr-bob-kou <bob323565@gmail.com>
2024-12-14 23:36:58 +0800

    Update 2_➕_Adding_Some_Heritages.py

--- pages/2_➕_Adding_Some_Heritages.py
+++ pages/2_➕_Adding_Some_Heritages.py
@@ -8,11 +8,16 @@ st.set_page_config(layout="wide")
 
 st.title("Adding!!")
 st.write("Coming Soon......")
+yr_range=list(range(1900,2100))
 with st.form("my_form"):
     st.write("Inside the form")
     name = st.text_input("Name")
     country= st.text_input("Country")
+    year=st.selectbox("Inscribed Year",yr_range)
+    description=st.text_area("Description","NA")
 
     submitted = st.form_submit_button("Submit")
 if submitted:
     st.write("Name", name, "Country", country)
+    st.write("Year",year)
+    st.write("description",description)
