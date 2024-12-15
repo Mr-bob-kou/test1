9d64b777934b29b525572948c2a879a0da38d75e
Mr-bob-kou <bob323565@gmail.com>
2024-12-15 15:00:29 +0800

    Update 2_➕_Adding_Some_Heritages.py

--- pages/2_➕_Adding_Some_Heritages.py
+++ pages/2_➕_Adding_Some_Heritages.py
@@ -21,13 +21,13 @@ with st.form("my_form"):
         y_cord=st.text_input("Latitude")
         type=st.selectbox("Type",tp)
         danger=st.radio("Is this Heritage in Danger?",["Yes","No"])
-        if danger=="Yes":
+        if danger =="Yes":
             st.write("Coco")
-            dang_year=st.selectbox("Years",yr_range)
-            areaha=st.text_input("Area(ha)")
+            #dang_year=st.selectbox("Years",yr_range)
+            #areaha=st.text_input("Area(ha)")
         else:
             st.write("lol")
-            areaha=st.text_input("Area(ha)")
+            #areaha=st.text_input("Area(ha)")
     with co2:
         m=leafmap.Map()
         m.to_streamlit(width=500, height=500)
