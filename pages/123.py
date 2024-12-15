0a5231cd96cc72ae071dca6b0d5a482dbc7487d6
Mr-bob-kou <bob323565@gmail.com>
2024-12-15 16:06:02 +0800

    Update 123.py

--- pages/123.py
+++ pages/123.py
@@ -30,3 +30,23 @@ if danger2 =="Yes":
 else:
     st.write("lol")
           
+modal = Modal(
+    "Demo Modal", 
+    key="demo-modal",
+    
+    # Optional
+    padding=20,    # default value
+    max_width=744  # default value
+)
+
+open_modal = st.button("Open")
+if open_modal:
+    modal.open()
+
+if modal.is_open():
+    with modal.container():
+        st.write("Text goes here")
+
+        st.write("Some fancy text")
+        value = st.checkbox("Check me")
+        st.write(f"Checkbox checked: {value}")
