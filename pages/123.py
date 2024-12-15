3deb7b6488f8962e54179f4876113ed3cb229a72
Mr-bob-kou <bob323565@gmail.com>
2024-12-15 14:59:03 +0800

    Update 123.py

--- pages/123.py
+++ pages/123.py
@@ -1,20 +1,16 @@
 import streamlit as st
-import leafmap.foliumap as leafmap
-import folium
-from folium import GeoJson
-import geopandas as gpd
-m = leafmap.Map(center=[51.505, -0.09], zoom=13)
 
-# 添加一個標記
-marker = m.add_marker([51.505, -0.09], popup="Hello, world!")
+genre = st.radio(
+    "What's your favorite movie genre",
+    [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
+    captions=[
+        "Laugh out loud.",
+        "Get the popcorn.",
+        "Never stop learning.",
+    ],
+)
 
-# 定義回調函數，當用戶點擊標記時執行
-def on_marker_click(event):
-    popup_content = event['popup']  # 獲取點擊的標記內容
-    print(f"Marker clicked! Popup content: {popup_content}")
-
-# 註冊回調函數以處理標記點擊事件
-m.on_interaction(on_marker_click)
-
-# 顯示地圖
-m.to_streamlit(width=700)
+if genre == ":rainbow[Comedy]":
+    st.write("You selected comedy.")
+else:
+    st.write("You didn't select comedy.")
